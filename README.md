cf5b9a28557eb95f0627b10a560c0ff84193f9d0
Alex Bilbie <hello@alexbilbie.com>
2018-04-02 20:38:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# update-ssm
+
+A super simple CLI utility to update an EC2 Parameter Store parameter.
+
+Usage:
+
+```
+$ update-ssm param-name param-value
+```
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Println("Expected two arguments, the first is the SSM key, the secon dis the new value")
+		os.Exit(1)
+	}
+
+	key := os.Args[1]
+	value := os.Args[2]
+
+	s, _ := session.NewSession(&aws.Config{})
+	client := ssm.New(s)
+
+	_, err := client.PutParameter(&ssm.PutParameterInput{
+		Name:      aws.String(key),
+		Overwrite: aws.Bool(true),
+		Type:      aws.String(ssm.ParameterTypeString),
+		Value:     aws.String(value),
+	})
+
+	if err != nil {
+		fmt.Printf("SSM PutParamter error: %s\n", err.Error())
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
